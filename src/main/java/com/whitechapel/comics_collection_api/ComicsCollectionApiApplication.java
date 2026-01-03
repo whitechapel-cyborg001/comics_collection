80a4039df3789e84e0227984932639c2018f5f6b
whitechapel-cyborg001 <asinweb12@gmail.com>
2026-01-03 15:13:01 +0100

    first commit

---
+++ src/main/java/com/whitechapel/comics_collection_api/ComicsCollectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.whitechapel.comics_collection_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsCollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsCollectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whitechapel/comics_collection_api/ComicsCollectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitechapel.comics_collection_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsCollectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
